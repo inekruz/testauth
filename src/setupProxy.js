cbbe4493b393791306ea07491d9eab6c5d916d2b
inekruz <nekruz15ik@gmail.com>
2024-07-05 19:32:16 +0500

    add proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/authapi',
+    createProxyMiddleware({
+      target: 'https://freefakeapi.io',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/authapi': '/authapi',
+      },
+    })
+  );
+};
